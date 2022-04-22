22cf9d94d37750e2db2e4a9f739c9c5041ac5357
Diego Anderson <dieanderson@gmail.com>
2022-04-21 22:34:28 -0300

    adicionado upload de imagens para o storage do firebase

--- src/store/actions/posts.js
+++ src/store/actions/posts.js
@@ -3,10 +3,23 @@ import axios from 'axios'
 import { ADD_POST, ADD_COMMENT } from './actionTypes'
 
 export const addPost = post => {
+    
     return dispatch => {
-        axios.post('/posts.json', { ...post })
+        axios({
+            url: 'uploadImage',
+            baseURL: 'https://us-central1-lambe-6cb0f.cloudfunctions.net',
+            method: 'POST',
+            data: {
+                image: post.image.base64
+            }
+        })
             .catch(err => console.log(err))
-            .then(res => console.log(res.data))
+            .then(res => {
+                post.image = res.data.imageUrl
+                axios.post('/posts.json', { ...post })
+                    .catch(err => console.log(err))
+                    .then(res => console.log(res.data))
+            })        
     }
     //return {
     //    type: ADD_POST,
